a397c404c0ffd395f424f37cb63453905bfaf85a
Maslikov Ilia <ilya.maslik@gmail.com>
2025-07-31 17:38:44 +0300

    add coratine

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -1,43 +1,66 @@
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent (typeof(HouseEvents))]
 public class Alarm : MonoBehaviour
 {
     [SerializeField] private AudioSource _alarmSound;
     [SerializeField] private float _volumeChangeSpeed = .5f;
 
     private HouseEvents _houseEvents;
-    private float _targetVolume = 0f;
+    private Coroutine _volumeCoroutine;
 
     private void Awake()
     {
         _houseEvents = GetComponent<HouseEvents>();
-        _alarmSound = GetComponentInChildren<AudioSource>();
 
-        _houseEvents.CrookEntered += ActivateAlarm;
-        _houseEvents.CrookExited += DeactivateAlarm;
+        _houseEvents.CrookEntered += OnCrookEntered;
+        _houseEvents.CrookExited += OnCrookExited;
     }
 
-    private void Update()
+    private void OnCrookEntered()
     {
-        _alarmSound.volume = Mathf.MoveTowards(_alarmSound.volume, _targetVolume, _volumeChangeSpeed * Time.deltaTime);
-    }
+        if (_volumeCoroutine != null)
+            StopCoroutine(_volumeCoroutine);
 
-    private void ActivateAlarm()
-    {
-        _targetVolume = 1f;
+        _volumeCoroutine = StartCoroutine(FadeVolume(1));
 
         if (_alarmSound.isPlaying == false)
             _alarmSound.Play();
     }
 
-    private void DeactivateAlarm()
+    private void OnCrookExited()
     {
-        _targetVolume = 0f;
+        if (_volumeCoroutine != null)
+            StopCoroutine(_volumeCoroutine);
+
+        _volumeCoroutine = StartCoroutine(FadeVolume(0));
+    }
+
+    private IEnumerator FadeVolume(float targetVolume)
+    {
+        float startVolume = _alarmSound.volume;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < _volumeChangeSpeed)
+        {
+            _alarmSound.volume = Mathf.MoveTowards(startVolume, targetVolume, elapsedTime/_volumeChangeSpeed); 
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        _alarmSound.volume = targetVolume;
+
+        if (Mathf.Approximately(targetVolume, 0f))
+            _alarmSound.Stop();
     }
 
     private void OnDestroy()
     {
-        _houseEvents.CrookEntered -= ActivateAlarm;
-        _houseEvents.CrookExited -= DeactivateAlarm;
+        _houseEvents.CrookEntered -= OnCrookEntered;
+        _houseEvents.CrookExited -= OnCrookExited;
+
+        if (_volumeCoroutine != null)
+            StopCoroutine(_volumeCoroutine);
     }
 }
